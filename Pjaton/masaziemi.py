64463e779fced4576d8211e27f7f11095066c6be
Al33ks <96834553+Al33ks@users.noreply.github.com>
2023-01-14 13:31:31 +0100

    Update masaziemi.py

--- Pjaton/masaziemi.py
+++ Pjaton/masaziemi.py
@@ -1,11 +1,14 @@
 import math
 
-# Wartości początkowe
-L = 2  # długość ramienia wahadła
-g = 9.81  # przyspieszenie ziemskie
-T = [2.70, 2.85, 2.82, 2.90, 2.87, 2.83, 2.76, 2.91, 2.65, 2.89, 2.88, 2,90]  # okres wahadła
+# stałe
+G = 6.67428e-11  # stała grawitacji
+M = 5.9722e24  # masa Ziemi
+R = 6371e3  # promień Ziemi
 
-# Obliczanie masy za pomocą wzoru
-m = (4 * math.pi**2 * L) / (g * T**2)
+# okres wahadła
+T = 2.83 * math.pi * math.sqrt(R / (G * M))
 
-print("Masa Ziemi:", m, "kg")
+# masa Ziemi
+mass = M
+
+print(mass)
