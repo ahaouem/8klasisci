b298d78da74460ee87c31c710c6d932ae5501f3b
Gamenight2008 <113185172+Gamenight2008@users.noreply.github.com>
2023-01-16 11:57:07 +0100

    jeszcze lepsza masa ziemi

--- Pjaton/masaziemi.py
+++ Pjaton/masaziemi.py
@@ -1,8 +1,10 @@
 import math
+import numpy as np
 
 def massOfEarthUsingPendulum(lenghtOfPendulum, fallTime, earthDiameter, gravitationalConstant):
-    g = (math.pi**2 * 2/fallTime ** 2) / lenghtOfPendulum * 2
-    massOfEarth = g * (math.pow(earthDiameter, 2) /gravitationalConstant)
+    #g = (((6.67 * 10**-11) * (5.972 * 10**24)) / (6371 * 1000)**2)
+    g = (4*math.pi**2 * lenghtOfPendulum) / (fallTime**2)
+    massOfEarth = g * ((earthDiameter)**2 / gravitationalConstant)
     return massOfEarth
 
 def fallOfPendulum(lenghtOfPendulum, gravity):
@@ -11,8 +13,14 @@ def fallOfPendulum(lenghtOfPendulum, gravity):
 
 earthDiameter = 6378.1 * 1000
 gravitationalConstant = 6.67408 * (10 ** -11)
-lenghtOfPendulum = 1
-g = 9.81
+lenghtOfPendulum = 0.5
+fallTime = 1.4179000000000004
+listaPomiarow = [1.4, 1.39, 1.42, 1.4, 1.44, 1.4, 1.44, 1.41, 1.44, 1.39, 1.4, 1.42, 1.41, 1.44, 1.42, 1.42, 1.44, 1.41, 1.41, 1.44, 1.4, 1.41, 1.43, 1.41, 1.44, 1.43, 1.44, 1.43, 1.42, 1.42, 1.41, 1.43, 1.4, 1.44, 1.43, 1.41, 1.4, 1.39, 1.4, 1.4, 1.42, 1.43, 1.43, 1.4, 1.4, 1.43, 1.41, 1.43, 1.4, 1.42, 1.43, 1.4, 1.4, 1.43, 1.43, 1.39, 1.44, 1.41, 1.43, 1.41, 1.41, 1.42, 1.41, 1.43, 1.42, 1.43, 1.44, 1.4, 1.4, 1.44, 1.43, 1.4, 1.39, 1.41, 1.42, 1.44, 1.41, 1.43, 1.43, 1.44, 1.41, 1.44, 1.41, 1.43, 1.43, 1.44, 1.43, 1.44, 1.42, 1.43, 1.4, 1.4, 1.39, 1.4, 1.44, 1.41, 1.42, 1.43, 1.41, 1.39]
 
-fallOfPendulum(lenghtOfPendulum, g)
-print(str(massOfEarthUsingPendulum(lenghtOfPendulum, fallOfPendulum(lenghtOfPendulum, g), earthDiameter, gravitationalConstant)) + "kg")
+x = sum(listaPomiarow)/len(listaPomiarow)
+odchylenie = np.std(listaPomiarow)
+
+print("Wszystkie pomary:", listaPomiarow)
+print("Błąd pomiarowy:", odchylenie)
+print("Średnia:", x)
+print("Masa ziemi:", massOfEarthUsingPendulum(lenghtOfPendulum, fallTime, earthDiameter, gravitationalConstant))
